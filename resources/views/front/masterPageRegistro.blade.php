55c82ed5ed1fbb250e47619b9307f735fa7e7082
renato lopez <lopez.renato@gmail.com>
2015-10-19 15:14:02 -0500

    Primera pantalla de registro

--- resources/views/front/masterPageRegistro.blade.php
+++ resources/views/front/masterPageRegistro.blade.php
@@ -4,126 +4,186 @@
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 
-	<head>
-
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<title>{{ trans('front/site.title') }}</title>
-		<meta name="description" content="">	
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-
-		@yield('head')
-
+ <head>
+    <meta charset="utf-8">
+    <title>Step by step tab style form validation Wizard using Bootstrap</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <style type="text/css">
+body {
+	margin-top:0px;
+}
+.stepwizard-step p {
+	margin-top: 10px;
+}
+.stepwizard-row {
+	display: table-row;
+}
+.stepwizard {
+	display: table;
+	width: 50%;
+	position: relative;
+}
+.stepwizard-step button[disabled] {
+	opacity: 1 !important;
+	filter: alpha(opacity=100) !important;
+}
+.stepwizard-row:before {
+	top: 14px;
+	bottom: 0;
+	position: absolute;
+	content: " ";
+	width: 100%;
+	height: 1px;
+	background-color: #ccc;
+	z-order: 0;
+}
+.stepwizard-step {
+	display: table-cell;
+	text-align: center;
+	position: relative;
+}
+.btn-circle {
+	width: 30px;
+	height: 30px;
+	text-align: center;
+	padding: 6px 0;
+	font-size: 12px;
+	line-height: 1.428571429;
+	border-radius: 15px;
+}
+</style>
 		{!! HTML::style('css/masterPagesRegistro.css') !!}
-		{!! HTML::style('css/jquery.steps.css') !!}
-		{!! HTML::style('css/main.css') !!}
-		{!! HTML::style('css/normalize.css') !!}
 
-		<!--[if (lt IE 9) & (!IEMobile)]>
-			{!! HTML::script('js/vendor/respond.min.js') !!}
-		<![endif]-->
-		<!--[if lt IE 9]>
-			{!! HTML::style('https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.js') !!}
-			{!! HTML::style('https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js') !!}
-		<![endif]-->
-
-		{!! HTML::style('http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800') !!}
-		{!! HTML::style('http://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic') !!}
-
-	</head>
-
-  <body>
-
-	<!--[if lt IE 8]>
-		<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-	<![endif]-->
-
+    <script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+</head>
+    
+    
+<body>
 	<header role="banner">
 		<div id="banner-principal">
 			<div id="logo"></div>
 		</div>
-		@yield('header')	
+		<div id="menu"></div>
 	</header>
-	<main role="main" class="container">
-		@if(session()->has('ok'))
-			@include('partials/error', ['type' => 'success', 'message' => session('ok')])
-		@endif	
-		@if(isset($info))
-			@include('partials/error', ['type' => 'info', 'message' => $info])
-		@endif
-		@yield('main')
-	</main>
-		<div class="container">
-			<div id="menu"></div>
-			<div class="contenido">
-				{!! Form::open(['url' => 'auth/login', 'method' => 'post', 'role' => 'form', 'id'=>'registro'] ) !!}
-				<div>
-        			<h3>Servicios y Eventos</h3>
-			        <section>
-			            <label for="userName">User name *</label>
-			            <input id="userName" name="userName" type="text" class="required">
-			            <label for="password">Password *</label>
-			            <input id="password" name="password" type="text" class="required">
-			            <label for="confirm">Confirm Password *</label>
-			            <input id="confirm" name="confirm" type="text" class="required">
-			            <p>(*) Mandatory</p>
-			        </section>        			
-        			<h3>Catalogos</h3>
-			        <section>
-			            <label for="name">First name *</label>
-			            <input id="name" name="name" type="text" class="required">
-			            <label for="surname">Last name *</label>
-			            <input id="surname" name="surname" type="text" class="required">
-			            <label for="email">Email *</label>
-			            <input id="email" name="email" type="text" class="required email">
-			            <label for="address">Address</label>
-			            <input id="address" name="address" type="text">
-			            <p>(*) Mandatory</p>
-			        </section>        			
-        			<h3>Resumen</h3>
-			        <section>
-			            <ul>
-			                <li>Foo</li>
-			                <li>Bar</li>
-			                <li>Foobar</li>
-			            </ul>
-			        </section>
-        			<h3>Finalizar</h3>
-			        <section>
-			            <input id="acceptTerms" name="acceptTerms" type="checkbox" class="required"> <label for="acceptTerms">I agree with the Terms and Conditions.</label>
-			        </section>
-        		</div>
-    			{!! Form::close() !!}    			
-			</div>
-		</div>
-	
-	<footer role="contentinfo">
-		 @yield('footer')
-		<p class="text-center"><small>Copyright &copy; Adrian</small></p>
-	</footer>
-		
-	
-	{!! HTML::script('js/jquery-1.9.1.min.js') !!}
-	{!! HTML::script('js/plugins.js') !!}
-	{!! HTML::script('js/modernizr-2.6.2.min.js') !!}
-	{!! HTML::script('js/jquery.cookie-1.3.1.js') !!}
-	{!! HTML::script('js/jquery.steps.js') !!}
-	{!! HTML::script('js/registro.js') !!}
-<!--  	
-	<script src="https://apis.google.com/_/scs/apps-static/_/js/k=oz.gapi.es_419.I2fUUel9E0s.O/m=auth/exm=plusone/rt=j/sv=1/d=1/ed=1/am=AQ/rs=AGLTcCMMtpYd3FTOpeq-jKZLL11dKGpUEg/t=zcms/cb=gapi.loaded_1" async="" />
-	<script src="https://apis.google.com/_/scs/apps-static/_/js/k=oz.gapi.es_419.I2fUUel9E0s.O/m=plusone/rt=j/sv=1/d=1/ed=1/am=AQ/rs=AGLTcCMMtpYd3FTOpeq-jKZLL11dKGpUEg/t=zcms/cb=gapi.loaded_0" async="" />
-	<script id="_carbonads_projs" type="text/javascript" src="//srv.carbonads.net/ads/C6AILKT.json?segment=placement:jquerystepscom&callback=_carbonads_go" />
-	
-	<script>
-		(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-		function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-		e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-		e.src='//www.google-analytics.com/analytics.js';
-		r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-		ga('create','UA-XXXXX-X');ga('send','pageview');		
-	</script>
--->
-	@yield('scripts')
 
-  </body>
+
+<div class="container">
+
+      <div class="stepwizard col-md-offset-3">
+      	<div class="stepwizard-row setup-panel">
+      		<div class="stepwizard-step">
+        		<a href="#step-1" type="button" class="btn btn-primary btn-circle">1</a>
+        		<p>Servicios y Eventos</p>
+      		</div>
+      		<div class="stepwizard-step">
+        		<a href="#step-2" type="button" class="btn btn-default btn-circle" disabled="disabled">2</a>
+        		<p>Catalogos</p>
+      		</div>
+      		<div class="stepwizard-step">
+        		<a href="#step-3" type="button" class="btn btn-default btn-circle" disabled="disabled">3</a>
+        		<p>Resumen</p>
+      		</div>
+	    	<div class="stepwizard-step">
+        		<a href="#step-4" type="button" class="btn btn-default btn-circle" disabled="disabled">4</a>
+        		<p>Finalizar</p>
+      		</div>
+      	</div>
+      </div>
+  </div>
+  {!! Form::open(['url' => 'auth/login', 'method' => 'post', 'role' => 'form', 'id'=>'registro'] ) !!}
+  <div class="row setup-content" id="step-1">
+    	<div class="col-xs-6 col-md-offset-3">
+        	<div class="col-md-12">
+            	<h3> Servicios y Eventos</h3>
+            	<div class="form-group">
+            		<label class="control-label">First Name</label>
+            		<input  maxlength="100" type="text" required="required" class="form-control" placeholder="Enter First Name"  />
+          		</div>
+              <div class="form-group">
+            <label class="control-label">Last Name</label>
+            <input maxlength="100" type="text" required="required" class="form-control" placeholder="Enter Last Name" />
+          </div>
+              <div class="form-group">
+            <label class="control-label">Address</label>
+            <textarea required="required" class="form-control" placeholder="Enter your address" ></textarea>
+          </div>
+              <button class="btn btn-primary nextBtn btn-lg pull-right" type="button" >Next</button>
+            </div>
+      </div>
+        </div>
+    <div class="row setup-content" id="step-2">
+          <div class="col-xs-6 col-md-offset-3">
+        <div class="col-md-12">
+              <h3> Step 2</h3>
+              <div class="form-group">
+            <label class="control-label">Company Name</label>
+            <input maxlength="200" type="text" required="required" class="form-control" placeholder="Enter Company Name" />
+          </div>
+              <div class="form-group">
+            <label class="control-label">Company Address</label>
+            <input maxlength="200" type="text" required="required" class="form-control" placeholder="Enter Company Address"  />
+          </div>
+              <button class="btn btn-primary nextBtn btn-lg pull-right" type="button" >Next</button>
+            </div>
+      </div>
+        </div>
+    <div class="row setup-content" id="step-3">
+          <div class="col-xs-6 col-md-offset-3">
+        <div class="col-md-12">
+              <h3> Step 3</h3>
+              <button class="btn btn-success btn-lg pull-right" type="submit">Submit</button>
+            </div>
+      </div>
+        </div>
+  {!! Form::close() !!}
+  </div>
+
+
+<script type="text/javascript">
+  $(document).ready(function () {
+  var navListItems = $('div.setup-panel div a'),
+		  allWells = $('.setup-content'),
+		  allNextBtn = $('.nextBtn');
+
+  allWells.hide();
+
+  navListItems.click(function (e) {
+	  e.preventDefault();
+	  var $target = $($(this).attr('href')),
+			  $item = $(this);
+
+	  if (!$item.hasClass('disabled')) {
+		  navListItems.removeClass('btn-primary').addClass('btn-default');
+		  $item.addClass('btn-primary');
+		  allWells.hide();
+		  $target.show();
+		  $target.find('input:eq(0)').focus();
+	  }
+  });
+
+  allNextBtn.click(function(){
+	  var curStep = $(this).closest(".setup-content"),
+		  curStepBtn = curStep.attr("id"),
+		  nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
+		  curInputs = curStep.find("input[type='text'],input[type='url'],textarea[textarea]"),
+		  isValid = true;
+
+	  $(".form-group").removeClass("has-error");
+	  for(var i=0; i<curInputs.length; i++){
+		  if (!curInputs[i].validity.valid){
+			  isValid = false;
+			  $(curInputs[i]).closest(".form-group").addClass("has-error");
+		  }
+	  }
+
+	  if (isValid)
+		  nextStepWizard.removeAttr('disabled').trigger('click');
+  });
+
+  $('div.setup-panel div a.btn-primary').trigger('click');
+});
+  </script>
+</body>
 </html>
