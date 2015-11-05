89e156913564f4912c5ac1cb2a980824c0b851b6
renato lopez <lopez.renato@gmail.com>
2015-11-05 14:49:04 -0500

    router

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -68,33 +68,14 @@ Route::controllers([
 
 
 
-
-//servicios
-Route::get('servicios', 'ServicioController@index');
-<<<<<<< HEAD
-=======
-
-Route::get('userservice', 'HomeController@index1');
-
-//Route::get('servicios/tipoOperador', 'ServicioController@step2');
->>>>>>> 2b2485fd2e840ba6cc54134b4a06e834f9d53884
-
-
-Route::post('servicios/tipoOperador', ['as' => 'upload-postTipoOperador', 'uses' =>'ServicioController@postTipoOperadores']);
-<<<<<<< HEAD
-Route::get('servicios/operador/{tipoOperador}', 'ServicioController@step2');
-Route::post('servicios/operador', ['as' => 'upload-postoperador', 'uses' =>'ServicioController@postOperadores']);
-Route::get('servicios/operadorServicios', 'ServicioController@step3');
-=======
-Route::post('servicios/operador', ['as' => 'upload-postservicios', 'uses' =>'ServicioController@postOperadores']);
-
-Route::get('step2', 'ServicioController@stepnum2');
-
-Route::post('servicios/servicioOperador', ['as' => 'upload-postServicioOperador', 'uses' =>'UsuarioServiciosController@postServicioOperadores']);
-
-Route::get('/image/{id}', ['as' => 'upload', 'uses' => 'ImageController@getUpload']);
+Route::get('/image', ['as' => 'upload', 'uses' => 'ImageController@getUpload']);
 Route::post('upload', ['as' => 'upload-post', 'uses' =>'ImageController@postUpload']);
 Route::post('upload/delete', ['as' => 'upload-remove', 'uses' =>'ImageController@deleteUpload']);
 
+//servicios
+Route::get('servicios', 'ServicioController@index');
 
->>>>>>> 2b2485fd2e840ba6cc54134b4a06e834f9d53884
+Route::post('servicios/tipoOperador', ['as' => 'upload-postTipoOperador', 'uses' =>'ServicioController@postTipoOperadores']);
+Route::get('servicios/operador/{tipoOperador}', 'ServicioController@step2');
+Route::post('servicios/operador', ['as' => 'upload-postoperador', 'uses' =>'ServicioController@postOperadores']);
+Route::get('servicios/operadorServicios', 'ServicioController@step3');
