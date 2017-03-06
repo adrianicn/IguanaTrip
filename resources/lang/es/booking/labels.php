ebfd4f57b4f8a05fea686b8af8ffb395bbd74e6e
Paredesfabio <fabio.paredes@gmail.com>
2017-03-06 16:41:18 -0500

    Etiquetas Espanol Booking

---
+++ resources/lang/es/booking/labels.php
@@ -0,0 +1,47 @@
+<?php
+
+return [
+	'label1' => 'Nombre Operador',
+	'label2' => 'Empresa Operador',
+	'label3' => 'Dirección Operador',
+	'label4' => 'Teléfono Operador',
+	'label5' => 'Correo Operador',
+	'label6' => 'Información del Operador',
+	'label7' => 'Información de la Reservación',
+	'label8' => 'Código de la Reservación',
+	'label9' => 'Gracias por su compra y por utilizar el Sistema de Booking',
+	'label10' => 'Su reserva ha sido confirmada!',
+	'label11' => 'Gracias por Preferirnos!',
+      	'label12' => 'Nombre Cliente',
+    	'label13' => 'Servicio Reservado',
+    	'label14' => 'Correo Cliente',
+    	'label15' => 'Reserva Desde',
+    	'label16' => 'Resrva Hasta',
+        'label17' => 'Teléfono Cliente',
+        'label18' => 'Número de Adultos',
+        'label19' => 'Número de Niños',
+        'label20' => 'Precio Total Reservación',
+        'label21' => 'Forma de Pago',
+        'label22' => 'Imprimir',
+        'label23' => 'Volver',
+        'label24' => 'Estimado usuario pedimos disculpas.',
+        'label25' => 'Algo Salio Mal',
+        'label26' => 'No tiene Acceso a esta parte del sistema',
+        'label28' => 'Volver Pagina Principal',
+        'label29' => 'El pago de su Reservación no se pudo confirmar. Por Favor Ponerse en contacto con el operador del servicio.',
+        'label30' => 'Su reserva no ha podido ser confirmada!',
+        'label31' => 'Error Confirmacion Pago Paypal',
+        'label32' => 'Confirmacion Pago Paypal',
+        'label33' => 'Error Confirmacion Pago Tarjeta de Credito',
+        'label34' => 'Confirmacion Tarjeta de Credito',
+        'label35' => 'Confirmacion Pago con Efectivo',
+        'label36' => 'Gracias por su compra, para confirmar su reserva debe comunicarse con el operador del servicio',
+        'label37' => 'Falta confirmación de la Reserva.',
+        'label38' => 'Efectivo',
+        'label39' => 'Tarjeta de Credito',
+    
+    
+    
+    
+        
+];
