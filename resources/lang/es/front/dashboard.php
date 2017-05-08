61cdf9ca1c12da7103e59e3491557d969addef4b
Paredesfabio <fabio.paredes@gmail.com>
2017-05-08 18:14:31 -0500

    Dashboard

---
+++ resources/lang/es/front/dashboard.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+	'connection' => 'Conexión',
+	'text' => 'Para iniciar sesión en este sitio debe rellenar este formulario:',
+	'email' => 'Tu email',
+	'password' => 'Tu password',
+	'remind' => 'Recordarme',
+	'forget' => 'Olvide mi contraseña!',
+	'register' => 'No estás registrado ?',
+	'register-info' => 'Para registrarse rápidamente, basta con hacer clic en el botón!',
+	'registering' => 'I subscribe',
+	'credentials' => 'Estas credenciales no coinciden con nuestros registros.',
+	'log' => 'Tu email o usuario',
+	'maxattempt' => 'Ha alcanzado el número máximo de intentos de inicio de sesión. Vuelve a intentarlo en un minuto.',
+        'logbtn' => 'Ingresar',
+        'title' => 'Dashboard',
+        'breadcumbs' => "Dasboard",
+        'saludo' => "Hola",
+        'texto' => "Desde el panel de control de Mi cuenta tiene la capacidad de ver una instantánea de la actividad de su cuenta reciente y actualizar la información de su cuenta. Seleccione un enlace para ver o editar información.",
+        'menu1' => "Dashboard",
+        'menu2' => "Mi Informacion",
+        'menu3' => "Agregar Pagina",
+        'menu4' => "Administrar Servicios",
+        'menu4' => "Contactanos",
+    
+        
+];
