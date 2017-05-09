0e7e88381a7ca621de23adadcb550569e904ac8e
Paredesfabio <fabio.paredes@gmail.com>
2017-05-09 16:40:48 -0500

    Etiquetas en Ingles Dashboard

---
+++ resources/lang/en/front/loginres.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+	'connection' => 'Connection',
+	'text' => 'To log on this site you must fill this form :',
+	'email' => 'Your email',
+	'password' => 'Your password',
+	'remind' => 'Remember me',
+	'forget' => 'Forgot your password?',
+	'register' => 'Not registered ?',
+	'register-info' => 'To register quickly just click on the button !',
+	'registering' => 'I subscribe',
+	'credentials' => 'These credentials do not match our records.',
+	'log' => 'Your email or your user name',
+	'maxattempt' => 'You have reached the maximum number of login attempts. Try again in one minute.',
+    	'logbtn' => 'LogIn',
+        'title' => 'Stop being a tourist, become a traveler.',
+        'breadcumbs' => "Login / Register",
+        'ingresocuenta' => "Login to your account"
+    
+];
---
+++ resources/lang/en/front/registerres.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+	'title' => 'Inscription',
+        'register' => 'Registration',
+	'infos' => "It's completely free",
+	'email' => 'Email',
+	'pseudo' => 'Name & LastName',
+	'password' => 'Password',
+	'confirm-email' => 'Confirm your email',
+	'warning' => 'Attention',
+	'warning-name' => '30 characters maximum',
+	'warning-password' => 'At least 8 characters',
+	'ok' => 'You have been registered !',
+	'error'=> 'These credentials do not match our records.',
+    'usertypepersona'=> 'Personal',
+    'usertypecompany'=> 'Company'
+    
+];
